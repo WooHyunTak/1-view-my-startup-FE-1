9fbd21126bfc57bbd3bd603fc46adb653cad5a71
sanho <dieuaimer@gmail.com>
2024-08-27 18:32:01 +0900

    style: Update CSS styles on company detail page and change text color based on input focus state in update modal

--- src/components/UpdateModal/UpdateModal.jsx
+++ src/components/UpdateModal/UpdateModal.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 function UpdateModal({ onUpdateConfirm, onCancel, password, initialAmount, initialComment }) {
   const [amount, setAmount] = useState(initialAmount || "");
   const [comment, setComment] = useState(initialComment || "");
+  const [isAmountFocused, setIsAmountFocused] = useState(false);
+  const [isCommentFocused, setIsCommentFocused] = useState(false);
 
   // 컴포넌트가 마운트될 때 초기 값을 설정
   useEffect(() => {
@@ -31,14 +33,20 @@ function UpdateModal({ onUpdateConfirm, onCancel, password, initialAmount, initi
         <input
           type="text"
           value={amount}
+          onFocus={() => setIsAmountFocused(true)}
+          onBlur={() => setIsAmountFocused(false)}
           onChange={(e) => setAmount(e.target.value)}
           placeholder="투자 금액을 입력해 주세요"
+          className={isAmountFocused || !amount ? "input-active" : "input-filled"}
         />
         <label>투자 코멘트</label>
         <textarea
           value={comment}
+          onFocus={() => setIsCommentFocused(true)}
+          onBlur={() => setIsCommentFocused(false)}
           onChange={(e) => setComment(e.target.value)}
           placeholder="투자에 대한 코멘트를 입력해 주세요"
+          className={isCommentFocused || !comment ? "input-active" : "input-filled"}
         />
         <div className="body-button">
           <button className="button-update" onClick={handleUpdateClick}>
