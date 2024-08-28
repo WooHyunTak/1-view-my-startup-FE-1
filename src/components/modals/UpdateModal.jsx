58e1d31b2e80bd1d6b924a3c1f0a163c5ca0a5e5
sanho <dieuaimer@gmail.com>
2024-08-28 14:08:21 +0900

    refactor: Replace <div> tags with <dialog> tags in modal components and combine modal-related CSS into globalModal.css

--- src/components/modals/UpdateModal.jsx
+++ src/components/modals/UpdateModal.jsx
@@ -1,19 +1,13 @@
-import "./UpdateModal.css";
 import cancelBtn from "../../assets/icon/ic_delete.svg";
+import { useState, useEffect, useRef } from "react";
+import "./globalModal.css";
 
-import { useState, useEffect } from "react";
-
-function UpdateModal({
-  onUpdateConfirm,
-  onCancel,
-  password,
-  initialAmount,
-  initialComment,
-}) {
+function UpdateModal({ isOpen, onUpdateConfirm, onCancel, password, initialAmount, initialComment }) {
   const [amount, setAmount] = useState(initialAmount || "");
   const [comment, setComment] = useState(initialComment || "");
   const [isAmountFocused, setIsAmountFocused] = useState(false);
   const [isCommentFocused, setIsCommentFocused] = useState(false);
+  const dialogRef = useRef(null);
 
   // 컴포넌트가 마운트될 때 초기 값을 설정
   useEffect(() => {
@@ -21,21 +15,34 @@ function UpdateModal({
     setComment(initialComment || "");
   }, [initialAmount, initialComment]);
 
+  // 모달을 열거나 닫는 효과를 제어
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (isOpen) {
+      dialog.showModal(); // 모달 열기
+      dialog.querySelector("input, textarea, button").blur(); // 첫 번째 포커스된 요소의 포커스 제거
+    } else {
+      dialog.close(); // 모달 닫기
+    }
+
+    // 컴포넌트가 언마운트될 때 모달을 닫아줍니다.
+    return () => {
+      if (dialog) {
+        dialog.close(); // 모달 닫기
+      }
+    };
+  }, [isOpen]);
+
   const handleUpdateClick = () => {
     onUpdateConfirm({ amount, comment, password });
   };
 
   return (
-    <div className="UpdateModal">
+    <dialog ref={dialogRef} className="UpdateModal" tabindex="-1">
       <div className="updateModal-head">
         <div className="head-title">
           <h3>수정하기</h3>
-          <img
-            className="cancel-button"
-            src={cancelBtn}
-            alt="cancel button img"
-            onClick={onCancel}
-          />
+          <img className="cancel-button" src={cancelBtn} alt="cancel button img" onClick={onCancel} />
         </div>
       </div>
 
@@ -48,9 +55,7 @@ function UpdateModal({
           onBlur={() => setIsAmountFocused(false)}
           onChange={(e) => setAmount(e.target.value)}
           placeholder="투자 금액을 입력해 주세요"
-          className={
-            isAmountFocused || !amount ? "input-active" : "input-filled"
-          }
+          className={isAmountFocused || !amount ? "input-active" : "input-filled"}
         />
         <label>투자 코멘트</label>
         <textarea
@@ -59,9 +64,7 @@ function UpdateModal({
           onBlur={() => setIsCommentFocused(false)}
           onChange={(e) => setComment(e.target.value)}
           placeholder="투자에 대한 코멘트를 입력해 주세요"
-          className={
-            isCommentFocused || !comment ? "input-active" : "input-filled"
-          }
+          className={isCommentFocused || !comment ? "input-active" : "input-filled"}
         />
         <div className="body-button">
           <button className="button-update" onClick={handleUpdateClick}>
@@ -69,7 +72,7 @@ function UpdateModal({
           </button>
         </div>
       </div>
-    </div>
+    </dialog>
   );
 }
 
